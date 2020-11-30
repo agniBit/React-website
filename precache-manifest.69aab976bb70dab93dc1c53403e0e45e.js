f08c21984d8abfc37dc9376e19b0da3441a75ec7
Abhishek Agnihotri <agni.abhi00@gmail.com>
2020-11-30 22:35:04 +0530

    Updates

---
+++ precache-manifest.69aab976bb70dab93dc1c53403e0e45e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65bdb6234274cc7b6af15b7e004726c",
+    "url": "/React-website/index.html"
+  },
+  {
+    "revision": "62a0ae3eb9b5c2c07668",
+    "url": "/React-website/static/css/main.689bcd34.chunk.css"
+  },
+  {
+    "revision": "0fb233d856b527a95dc6",
+    "url": "/React-website/static/js/2.7fb9f708.chunk.js"
+  },
+  {
+    "revision": "5b53417006fc3feb1d9b4d13035672d5",
+    "url": "/React-website/static/js/2.7fb9f708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a0ae3eb9b5c2c07668",
+    "url": "/React-website/static/js/main.c884f991.chunk.js"
+  },
+  {
+    "revision": "413e28ec29a30c9c5cf1",
+    "url": "/React-website/static/js/runtime-main.3ccc85b2.js"
+  }
+]);
